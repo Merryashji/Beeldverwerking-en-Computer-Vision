4cd9628e189c4021b26bd78c4cef872a31e5eb3d
Merryashji <60436351+Merryashji@users.noreply.github.com>
2021-03-19 12:58:14 +0100

    Update face_mask_detector.py

--- face_mask_detector.py
+++ face_mask_detector.py
@@ -2,9 +2,14 @@ import numpy as np
 import os
 import matplotlib.pyplot as plt
 from imutils import paths
-import cv2
+import cv2 
 
 from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.utils import to_categorical
+
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
 
 
 image_paths=list(paths.list_images('C:/Users/Gebruiker/Downloads/data'))
@@ -22,3 +27,12 @@ def create_training_data():
         training_data.append(img )
 
 create_training_data()
+
+training_data=np.array(training_data,dtype='float32') 
+labels=np.array(labels)
+
+lb=LabelBinarizer() 
+labels=lb.fit_transform(labels)
+labels=to_categorical(labels)
+
+train_X,test_X,train_Y,test_Y=train_test_split(training_data,labels,test_size=0.30,stratify=labels,random_state=10)
